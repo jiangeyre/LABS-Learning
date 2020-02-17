b55c50a6c5024000d9fb5d88684e03739bcbc691
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-02-17 09:05:01 -0800

    Commit 3

--- server.js
+++ server.js
@@ -60,6 +60,11 @@ const RootQueryType = new GraphQLObjectType({
             type: new GraphQLList(BookType),
             description: 'List of Books',
             resolve: () => books
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            description: 'List of Authors',
+            resolve: () => authors
         }
     })
 });
