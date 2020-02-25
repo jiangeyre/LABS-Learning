0167a3e8e13f9c43ba7f1174271851771c357f49
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-02-25 09:29:29 -0800

    Feb 25, Commit 1

--- server.js
+++ server.js
@@ -79,9 +79,49 @@ const RootQueryType = new GraphQLObjectType({
             type: new GraphQLList(AuthorType),
             description: 'List of Authors',
             resolve: () => authors
+        },
+        author: {
+            type: AuthorType,
+            description: 'A single author',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parents, args) => authors.find(author => author.id === args.id)
         }
     })
 });
+
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields: () => ({
+        addBook: {
+            type: BookType,
+            description: 'Add a book.',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                authorId: { type: GraphQLNonNull(GraphQLInt) }
+            },
+            resolve: (parent, args) => {
+                const book = { id: books.length + 1, name: args.name, authorId: args.authorId }
+                books.push(book)
+                return book
+            }
+        },
+        addAuthor: {
+            type: AuthorType,
+            description: 'Add an author.',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve: (parent, args) => {
+                const author = { id: authors.length + 1, name: args.name }
+                authors.push(author)
+                return author
+            }
+        }
+    })
+})
 
 // const schema = new GraphQLSchema({
 //     query: new GraphQLObjectType({
@@ -96,7 +136,8 @@ const RootQueryType = new GraphQLObjectType({
 // })
 
 const schema = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMutationType
 });
 
 server.use('/graphql', expressGraphQL({
